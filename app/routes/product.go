76c6bb65a924c7a705201334c63495f66b550758
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-12-31 19:02:44 +0700

    tiny adjust

--- app/routes/product.go
+++ app/routes/product.go
@@ -11,10 +11,15 @@ import (
 func ProductRoute(api *gin.RouterGroup) {
 	product := api.Group("/product")
 	{
-		product.POST("", controllers.CreateProduct)       //POST is used to add new data
-		product.GET("", controllers.GetProducts)          //GET is used to retrieve data that has been stored
-		product.GET("/:id", controllers.GetProductByID)   //GET /:id is used to retrieve data that has been stored based on id
-		product.PATCH("/:id", controllers.UpdateProduct)  //PATCH /:id is used to change data that has been stored based on id
-		product.DELETE("/:id", controllers.DeleteProduct) //DELETE /:id is used to change data that has been stored based on id
+		//POST is used to add new data
+		product.POST("", controllers.CreateProduct)
+		//GET is used to retrieve data that has been stored
+		product.GET("", controllers.GetProducts)
+		//GET /:id is used to retrieve data that has been stored based on id
+		product.GET("/:id", controllers.GetProductByID)
+		//PATCH /:id is used to change data that has been stored based on id
+		product.PATCH("/:id", controllers.UpdateProduct)
+		//DELETE /:id is used to change data that has been stored based on id
+		product.DELETE("/:id", controllers.DeleteProduct)
 	}
 }
